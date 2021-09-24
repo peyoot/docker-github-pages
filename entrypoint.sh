28d70e0135a3bc099dbd736bc85f68c647da99a9
peyoot <peyoot@hotmail.com>
2021-09-23 23:57:40 -0500

    now env GITHUB_REPO to pull your own github pages repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,19 +7,21 @@ echo "check github_repo is ${GITHUB_REPO}"
 git config --global user.name "${GITHUB_USER}" 
 git config --global user.email "${GITHUB_EMAIL}"
 if [ ! -d /hexo/source/_posts ] ; then
-  echo "empty hexo-site without any source posts" 
+  echo "empty hexo-site without any source posts" ;
+  hexo init temp ;
   if [ -n "${GITHUB_REPO}" ] ; then 
       echo "you have specified the github repo" ;
-      git clone git@github.com:${GITHUB_USER}/${GITHUB_REPO} temp ; 
+      git clone git@github.com:${GITHUB_USER}/${GITHUB_REPO} temprepo ; 
+      cp -af temprepo/. temp
   else 
-      echo "no github repo provided, will generate a new hexo site" ;
-      hexo init temp ; 
+      echo "no github repo provided,a new hexo site generated" ;
+#      hexo init temp ; 
   fi ; 
 #  ls -A temp|xargs -i cp -rf temp/{} ./ ;
   ls -A temp|grep -v _config.yml |xargs -i cp -af temp/{} ./ ;
   echo "" > _config.yml ;
   cat temp/_config.yml >>_config.yml ;
-#  rm -rf temp ;
+  rm -rf temp* ;
 else
   echo "hexo-site with source posts mounted" ;
 fi ;
@@ -28,5 +30,5 @@ echo "hexo site is ready!"
 #hexo clean
 #hexo g
 #hexo d
-#hexo server
-top
+hexo server
+#top
